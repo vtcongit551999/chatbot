586e9e30125e8a934a82a59b70f672222d861875
cong <vtcong551999@gmail.com>
2021-05-23 10:22:03 +0700

    two change

--- chatbotapp/home/urls.py
+++ chatbotapp/home/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
+app_name='home'
 urlpatterns = [
-    path('',views.home,name='home'),
+    path( '', views.home, name='home'),
+    
 ]
---
+++ chatbotapp/rasachat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RasachatConfig(AppConfig):
+    name = 'rasachat'
