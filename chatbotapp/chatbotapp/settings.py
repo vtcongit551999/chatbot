291821275e546615f4deab160dfa98421f5ca981
cong <vtcong551999@gmail.com>
2021-06-02 23:19:52 +0700

    two changes

--- chatbotapp/chatbotapp/settings.py
+++ chatbotapp/chatbotapp/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static")
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
